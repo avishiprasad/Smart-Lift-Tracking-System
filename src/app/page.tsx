dfe041bb14740240061753f81da5d8f35c855f49
avishiprasad <prasadavishi@gmail.com>
2025-08-31 19:26:58 +0530

    dummy liftinfo.json data and displaying it in admin page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,12 @@
 import HeroSection from "@/components/HeroSection";
+import Navbar from "@/components/Navbar";
 
 export default function Home() {
   return (
     <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02] pt-24">
+      <div className="relative w-full flex items-center justify-center ">
+          <Navbar />
+      </div>
       <HeroSection />
     </main>
   );
